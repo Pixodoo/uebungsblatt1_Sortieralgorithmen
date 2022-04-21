063411cad7debfb52abe56504681cbaccc285d90
Pixodoo <92550112+Pixodoo@users.noreply.github.com>
2022-04-21 17:49:32 +0200

    Add files via upload

---
+++ Ex01_LastnamesOfMembers_project/src/test/java/de/unistuttgart/dsass2022/ex01/p5/BubbleSortCheckerList.java
@@ -0,0 +1,8 @@
+// SOLUTION
+package de.unistuttgart.dsass2022.ex01.p5;
+
+public class BubbleSortCheckerList<T extends Comparable<T>> extends AbstractSortCheckerList<T> {
+
+	
+
+}
---
+++ Ex01_LastnamesOfMembers_project/src/test/java/de/unistuttgart/dsass2022/ex01/p5/SorterTest.java
@@ -0,0 +1,20 @@
+package de.unistuttgart.dsass2022.ex01.p5;
+
+import static org.junit.Assert.*;
+
+// START SOLUTION
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+// END SOLUTION
+
+import org.junit.Test;
+
+import de.unistuttgart.dsass2022.ex01.p5.ISimpleList;
+import de.unistuttgart.dsass2022.ex01.p5.Sorter;
+
+public class SorterTest {
+
+	
+
+}
